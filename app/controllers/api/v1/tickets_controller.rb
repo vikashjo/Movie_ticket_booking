b59f18f4ff190c275169b4d578694f6bf8277dc4
vikashjo <vikashjoshi431@gmail.com>
2024-09-04 14:52:45 +0530

    HAndle race condition booking ticket

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -9,13 +9,25 @@ module Api
       end
 
       def create
-        @ticket = @showtime.tickets.new(ticket_params.merge(user:current_user))
-        if @ticket.save
-          render json: {message: "Congratulations! tickets booked successfully", data: @ticket}
-        else
-          render json: { errors: @ticket.errors.full_messages }, status: :unprocessable_entity
+
+      #   @ticket = @showtime.tickets.new(ticket_params.merge(user:current_user))
+      #   if @ticket.save
+      #     render json: {message: "Congratulations! tickets booked successfully", data: @ticket}
+      #   else
+      #     render json: { errors: @ticket.errors.full_messages }, status: :unprocessable_entity
+      #   end
+      # end
+        
+        ActiveRecord::Base.transaction do
+          if Ticket.exists?(showtime_id: @showtime.id, seat_number: params[:ticket][:seat_number])
+            render json: { message: "Seat is already booked, Please choose some other seat"}, status: :unprocessable_entity
+          else
+            @ticket = @showtime.tickets.create!(ticket_params.merge(user:current_user))
+            render json: {message: "Congratulations! tickets booked successfully", data: @ticket}, status: :created
+          end      
         end
       end
+
 
       private
 
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,11 +3,11 @@ class Ticket < ApplicationRecord
   belongs_to :user
 
   validates :seat_number, presence: true
-  validate :seat_availability
+  
 
-  def seat_availability
-    if Ticket.exists?(showtime_id: showtime_id, seat_number: seat_number)
-      errors.add(:seat_number,"#{seat_number} is already booked, Please choose some other seat")
-    end
+  # def seat_availability
+  #   if Ticket.exists?(showtime_id: showtime_id, seat_number: seat_number)
+  #     errors.add(:seat_number,"#{seat_number} is already booked, Please choose some other seat")
+  #   end
   end
 end
